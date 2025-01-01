150371dc4e5c7d723134a070c1b7f74e9e77c5bf
1221solomon-tadesse <solomontadesse480@gmail.com>
2025-01-01 23:10:55 +0300

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-// import Login from "@/components/Login"
 export default function Home() {
   return (
-    <div className="text-blue-600" >
-      {/* <Home/> */}
-     
-      {/* <Login/> */}
-    delivery app
-    vf
-    </div>
+    <main>
+      <div>
+        <h1 className="text-2xl font-serif font-bold">
+          this is the delivery app{" "}
+        </h1>
+        <p>Sample menus</p>
+        <div className="flex justify-around">
+          <div className="border-red-300 border-2 "><h1>burger</h1>
+          <p>price: </p>
+          </div>
+          <div className="border-red-300 border-2 "><h1>burger</h1>
+          <p>price: </p>
+          </div>
+          <div className="border-red-300 border-2 "><h1>burger</h1>
+          <p>price: </p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
